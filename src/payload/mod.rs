a8465edcb7c077f772c1422d566e541a09d248a4
Jens Saalmüller <jens.saalmueller@gmx.de>
2023-12-12 14:36:45 +0100

    feat: add output formats

--- src/payload/mod.rs
+++ src/payload/mod.rs
@@ -1,2 +1,32 @@
+use std::str::Utf8Error;
+
+use log::error;
+use protofish::context::ParseError;
+use thiserror::Error;
+
 pub mod protobuf;
 pub mod text;
+
+#[derive(Debug)]
+pub enum OutputFormat {
+    PLAIN,
+    JSON,
+    YAML,
+    CSV,
+}
+
+#[derive(Debug, Error)]
+pub enum PayloadError {
+    #[error("Output format {0:?} not supported")]
+    OutputFormatNotSupported(OutputFormat),
+    #[error("Could not convert payload to UTF 8 string")]
+    CouldNotConvertToUtf8(Utf8Error),
+    #[error("Could not open definition file {0}")]
+    CouldNotOpenDefinitionFile(String),
+    #[error("Could not parse proto file {0}")]
+    CouldNotParseProtoFile(ParseError),
+    #[error("Message {0} not found in proto file, cannot decode payload")]
+    MessageNotFoundInProtoFile(String),
+    #[error("Field with number {0} not found in proto file")]
+    FieldNumberNotFoundInProtoFile(u64),
+}
--- src/payload/text.rs
+++ src/payload/text.rs
@@ -1,18 +1,30 @@
 use std::str::from_utf8;
 
-use log::error;
 use rumqttc::v5::mqttbytes::v5::Publish;
+
+use crate::payload::{OutputFormat, PayloadError};
 
 pub struct PayloadTextHandler {}
 
 impl PayloadTextHandler {
-    pub fn handle_publish(value: &Publish) {
+    pub fn handle_publish(value: &Publish, output_format: OutputFormat) -> Result<Vec<u8>, PayloadError> {
         match from_utf8(value.payload.as_ref()) {
             Ok(content) => {
-                println!("{}", content);
+                Self::encode_to_output_format(content, output_format)
             }
             Err(e) => {
-                error!("Could not convert payload to UTF 8 string: {e:?}");
+                Err(PayloadError::CouldNotConvertToUtf8(e))
+            }
+        }
+    }
+
+    fn encode_to_output_format(content: &str, output_format: OutputFormat) -> Result<Vec<u8>, PayloadError> {
+        match output_format {
+            OutputFormat::PLAIN => {
+                Ok(content.to_string().into_bytes())
+            }
+            _ => {
+                Err(PayloadError::OutputFormatNotSupported(output_format))
             }
         }
     }
