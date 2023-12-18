cc8d09aee500b61b1a9bcaa0bcc7020c6e3a2e87
Jens Saalmüller <jens.saalmueller@codecentric.de>
2023-12-18 10:26:03 +0100

    feat: add publish triggers

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,18 @@ use tokio::sync::broadcast;
 use tokio::sync::broadcast::Sender;
 use tokio::task::JoinHandle;
 
-use crate::config::mqtli_config::parse_config;
+use crate::config::mqtli_config::{parse_config, Topic};
+use crate::config::mqtli_config::PublishTriggerType::Periodic;
 use crate::mqtt_handler::MqttHandler;
 use crate::mqtt_service::MqttService;
+use crate::publish::trigger_periodic::TriggerPeriodic;
 
 mod config;
 mod mqtt_service;
 mod mqtt_handler;
 mod payload;
 mod output;
+mod publish;
 
 
 #[tokio::main]
@@ -46,7 +49,8 @@ async fn main() {
 
     let (sender, receiver) = broadcast::channel(32);
 
-    let mut handler = MqttHandler::new(Arc::new(Box::new(config.topics)));
+    let topics = Arc::new(Box::new(config.topics));
+    let mut handler = MqttHandler::new(topics.clone());
     handler.start_task(receiver);
 
     if let Err(e) = mqtt_service.connect(Some(sender)).await {
@@ -54,9 +58,25 @@ async fn main() {
         exit(2);
     }
 
+    start_scheduler(topics.clone()).await;
+
     start_exit_task(sender_exit).await;
     mqtt_service.await_task().await;
     handler.await_task().await;
+}
+
+async fn start_scheduler(topics: Arc<Box<Vec<Topic>>>) {
+    let mut scheduler = TriggerPeriodic::new();
+
+    topics.iter().for_each(|topic| for trigger in topic.publish().trigger() {
+        if let Periodic(value) = trigger {
+            if let Err(e) = scheduler.add_schedule(value.interval(), value.count(), value.initial_delay()) {
+                error!("Error while adding schedule: {:?}", e);
+            };
+        }
+    });
+
+    scheduler.start().await;
 }
 
 async fn start_exit_task(sender_exit: Sender<i32>) {
