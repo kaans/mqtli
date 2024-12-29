a0e15d73f0a53becbffb12dab535b29294d2c53e
Jens Saalmüller <jens.saalmueller@gmx.de>
2024-12-29 20:16:30 +0100

    chore: move crates to new subdirectory

--- crates/mqtlib/build.rs
+++ crates/mqtlib/build.rs
@@ -4,9 +4,9 @@ fn main() {
         .protoc()
         // Use `protoc-bin-vendored` bundled protoc command, optional.
         .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
-        .includes(["../assets/protobuf"])
+        .includes(["assets/protobuf"])
         // Inputs must reside in some of include paths.
-        .input("../assets/protobuf/sparkplug_b.proto")
+        .input("assets/protobuf/sparkplug_b.proto")
         // Specify output directory relative to Cargo output directory.
         .cargo_out_dir("protos")
         .run_from_script();
